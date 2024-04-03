a33d3d73d2bd236c11cf920605eed372f46f993d
psytechbd <psytechbd@gmail.com>
2024-04-04 01:41:24 +0600

    added notifications

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('user-icon').addEventListener('click', function () {
+    document.getElementById('user-menu').classList.toggle('active');
+});
+document.getElementById('notification-icon').addEventListener('click', function () {
+    document.getElementById('user-notification').classList.toggle('active');
+});
